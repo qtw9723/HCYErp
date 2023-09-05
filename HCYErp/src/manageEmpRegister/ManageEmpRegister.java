aedf068f9ff21556de8084a6e92a6dd227dde95f
user <user@DESKTOP-COJ9858>
2023-09-05 15:46:37 +0900

    기다료봐

--- HCYErp/src/manageEmpRegister/ManageEmpRegister.java
+++ HCYErp/src/manageEmpRegister/ManageEmpRegister.java
@@ -1,5 +1,65 @@
 package manageEmpRegister;
 
-public class ManageEmpRegister {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class ManageEmpRegister extends JPanel{
+	private JButton jbtnEmpRegister;
+	private JButton jbtnResign;
+	private JButton jbtnAbsence;
+	private JTable jtRegiAbInfo;
+	private JButton jbtnLogOut;
+	private JLabel jlblLogoTxt;
+	private JScrollPane jspResignAb;
+	
+	public ManageEmpRegister() {
+		ManageEmpRegisterEvt event = new ManageEmpRegisterEvt(this);
+		
+		//입퇴사자 테이블
+		DefaultTableModel dtmRefiAbInfo = new DefaultTableModel();
+		dtmRefiAbInfo.addColumn("사원번호");
+		dtmRefiAbInfo.addColumn("사원이름");
+		dtmRefiAbInfo.addColumn("퇴사일");
+		dtmRefiAbInfo.addColumn("승인일");
+		dtmRefiAbInfo.addColumn("퇴사사유");
+		//다오에서 추가
+		List<Object[]> ojList = new ArrayList<Object[]>();
+		for(int i =0;i<100;i++) {
+			ojList.add(new Object[]{i,"이름","2023.09.05","2023.09.04","회장이 갈궈서"});
+		}//사사사사사사삭제
+		
+		for(Object[] oj:ojList) {
+		dtmRefiAbInfo.addRow(oj);
+		}//for
+		jtRegiAbInfo = new JTable(dtmRefiAbInfo);
+		jspResignAb = new JScrollPane(jtRegiAbInfo);
+		jspResignAb.setBounds(100,100,100,100);
+		
+		add(jspResignAb);
+		
+		
+		//로그아웃 버튼
+		jbtnLogOut = new JButton("로그아웃");
+		jbtnLogOut.setBounds(1000,510,150,40);
+		jbtnLogOut.addActionListener(event);
+		add(jbtnLogOut);
+		//텍스트 로고
+		jlblLogoTxt = new JLabel(new ImageIcon("C:/Users/user/git/HCYErp/HCYErp/src/image/HCYTextLogo.png"));
+		jlblLogoTxt.setBounds(930,450,300,300);
+		add(jlblLogoTxt);
+		
+		// 배경 설정
+		JLabel jlblBG = new JLabel(new ImageIcon("C:/Users/user/git/HCYErp/HCYErp/src/image/HCYErp배경.png"));
+		jlblBG.setBounds(0, 0, 1200, 700);
+		add(jlblBG);
+	}//constructor
 }//class
--- HCYErp/src/manageEmpRegister/ManageEmpRegisterEvt.java
+++ HCYErp/src/manageEmpRegister/ManageEmpRegisterEvt.java
@@ -1,11 +1,19 @@
 package manageEmpRegister;
 
-public class ManageEmpRegisterEvt {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+
+public class ManageEmpRegisterEvt extends MouseAdapter implements ActionListener {
 
 	private ManageEmpRegister mer;
 	
 	public ManageEmpRegisterEvt(ManageEmpRegister mer) {
 		this.mer=mer;
 	}//constructor
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
 
 }//class
