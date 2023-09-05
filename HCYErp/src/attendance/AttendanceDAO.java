9af8fb206f2043a794e2d8ffbcf20ca4621a298b
user <user@DESKTOP-COJ9858>
2023-09-05 17:49:08 +0900

    깨작깨작

--- HCYErp/src/attendance/AttendanceDAO.java
+++ HCYErp/src/attendance/AttendanceDAO.java
@@ -1,8 +1,17 @@
 package attendance;
 
-import login.HCYErp;
-
 public class AttendanceDAO {
-
+	private static AttendanceDAO attendanceDAO; 
+	
+	private AttendanceDAO() {
+		
+	}//constructor
+	
+	public static AttendanceDAO getInstance() {
+		if(attendanceDAO==null) {
+			attendanceDAO=new AttendanceDAO();
+		}//end if
+		return attendanceDAO;
+	}//getInstance
 	
 }//class
