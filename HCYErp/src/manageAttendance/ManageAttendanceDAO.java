feddfc1f393095dd7c4b18fb101fec3652d11196
user <user@DESKTOP-COJ9858>
2023-09-12 09:54:59 +0900

    매니지 어텐던스 끝

--- HCYErp/src/manageAttendance/ManageAttendanceDAO.java
+++ HCYErp/src/manageAttendance/ManageAttendanceDAO.java
@@ -6,17 +6,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import DB.DbConn;
 import VO.AttendanceVO;
+import attendance.AttendanceStatus;
 
 public class ManageAttendanceDAO {
 
 	private static ManageAttendanceDAO maDAO;
+	private static int WORK_START_TIME = 9;
 	
 	private ManageAttendanceDAO() { 
-		
 	}//Constructor
 	
 	public static ManageAttendanceDAO getInstance() {
@@ -61,8 +65,98 @@ public class ManageAttendanceDAO {
 		
 	}//selectNoAttend
 	
-	public List<AttendanceVO> selectNoAttend(int empno) throws SQLException{
-		List<AttendanceVO> list=new ArrayList<AttendanceVO>();
+	public Map<Integer, AttendanceStatus> selectNoAttend(int empno) throws SQLException {
+		Map<Integer, AttendanceStatus> attendance = new HashMap<Integer, AttendanceStatus>();
+		DbConn db = DbConn.getInstance();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = db.getConnection("192.168.10.145", "hcytravel", "boramsangjo");
+
+			// 휴가일 검색
+			String selectPersonalAttendance = "SELECT to_date(STARTDATE,'yyyy-mm-dd'), DAYOFFDAYS FROM DAYOFF_APPLY where empno = ? and APPROVE = 'Y'";
+
+			pstmt = con.prepareStatement(selectPersonalAttendance);
+			pstmt.setInt(1, empno);
+
+			rs = pstmt.executeQuery();
+			java.util.Date tempDate = null;
+			Calendar targetCal = Calendar.getInstance();
+			Calendar currentCal = Calendar.getInstance();
+			while (rs.next()) {
+				tempDate = new java.util.Date(rs.getDate(1).getTime());
+				targetCal.setTime(tempDate);
+				if (targetCal.get(Calendar.YEAR) == currentCal.get(Calendar.YEAR)
+						&& targetCal.get(Calendar.MONTH) == currentCal.get(Calendar.MONTH)) {
+					for (int i = 0; i < rs.getInt(2); i++) {
+						attendance.put(targetCal.get(Calendar.DAY_OF_MONTH), AttendanceStatus.DAY_OFF);
+					} // for
+				} // if
+			} // while
+
+			// 출근일 및 지각일 검색
+			rs.close();
+			pstmt.close();
+
+			selectPersonalAttendance = "select to_date(starttime,'hh:mi:ss'),workdate from ATTENDANCE where empno = ? and ENDTIME is not null";
+
+			pstmt = con.prepareStatement(selectPersonalAttendance);
+			pstmt.setInt(1, empno);
+
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				tempDate = new java.util.Date(rs.getDate(1).getTime());
+				targetCal.setTime(tempDate);
+				if (targetCal.get(Calendar.YEAR) == currentCal.get(Calendar.YEAR)
+						&& targetCal.get(Calendar.MONTH) == currentCal.get(Calendar.MONTH)) {
+					if (targetCal.get(Calendar.HOUR_OF_DAY) < WORK_START_TIME) {
+						tempDate = new java.util.Date(rs.getDate(2).getTime());
+						targetCal.setTime(tempDate);
+						attendance.put(targetCal.get(Calendar.DAY_OF_MONTH), AttendanceStatus.ATTENDANCE);
+					} else {
+						tempDate = new java.util.Date(rs.getDate(2).getTime());
+						targetCal.setTime(tempDate);
+						attendance.put(targetCal.get(Calendar.DAY_OF_MONTH), AttendanceStatus.ABSENCE);
+					} // else
+				} // if
+			} // while
+
+			// 휴직일 검색
+			rs.close();
+			pstmt.close();
+
+			selectPersonalAttendance = "SELECT STARTDATE, ABSENCEDAYS FROM ABSENCE where empno = ? ";
+
+			pstmt = con.prepareStatement(selectPersonalAttendance);
+			pstmt.setInt(1, empno);
+
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				System.out.println("하하");
+				tempDate = new java.util.Date(rs.getDate(1).getTime());
+				targetCal.setTime(tempDate);
+				for (int i = 0; i < rs.getInt(2); i++) {
+					if (targetCal.get(Calendar.YEAR) == currentCal.get(Calendar.YEAR)
+							&& targetCal.get(Calendar.MONTH) == currentCal.get(Calendar.MONTH)) {
+						attendance.put(targetCal.get(Calendar.DAY_OF_MONTH), AttendanceStatus.LEAVE);
+						targetCal.set(Calendar.DAY_OF_MONTH, targetCal.get(Calendar.DAY_OF_MONTH) + 1);
+					} else {
+						break;
+					} // else
+				} // for
+			} // while
+		} finally {
+			db.dbclose(rs, pstmt, con);
+		} // finally
+
+		return attendance;
+	}// selectPersonalAttendance
+	
+	public List<String> selectEmp() throws SQLException{
+		List<String> list = new ArrayList<String>();
+		
 		Connection con=null;
 		PreparedStatement pstmt=null;
 		ResultSet rs=null;
@@ -70,28 +164,23 @@ public class ManageAttendanceDAO {
 		DbConn db=DbConn.getInstance();
 		
 		try {
-			
 			con=db.getConnection("192.168.10.145", "hcytravel", "boramsangjo");
 			
-			String sql="select e.empno,e.ename,at.starttime,at.endtime,at.workdate from emp e,attendance at where (at.empno=e.empno) and e.empno=?";
+			String sql="SELECT EMPNO, ENAME FROM EMP";
 			
 			pstmt=con.prepareStatement(sql);
-			pstmt.setInt(1, empno);
 			rs=pstmt.executeQuery();
-			AttendanceVO atVO=null;
+			StringBuilder sbEmp = new StringBuilder();
 			while(rs.next()) {
-				atVO=new AttendanceVO();
-				atVO.setEmpNo(rs.getInt("empno"));
-				atVO.setEname(rs.getString("ename"));
-				atVO.setStartTime(rs.getString("starttime"));
-				atVO.setEndTime(rs.getString("endtime"));
-				atVO.setWorkDate(rs.getString("workdate"));
-				list.add(atVO);
+				sbEmp.replace(0, sbEmp.length(), "");
+				sbEmp.append(rs.getInt("empno")).append("/").append(rs.getString("ename"));
+				list.add(sbEmp.toString());
 			}//while
 			
 		}finally {
 			db.dbclose(rs, pstmt, con);
 		}//try
+		
 		return list;
-	}//selectNoAttend
+	}//selectEmp
 }//class
--- HCYErp/src/manageAttendance/ManagePersonalAttendanceEvt.java
+++ HCYErp/src/manageAttendance/ManagePersonalAttendanceEvt.java
@@ -1,21 +1,115 @@
 package manageAttendance;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class ManagePersonalAttendanceEvt extends MouseAdapter implements ActionListener{
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import attendance.AttendanceStatus;
+
+public class ManagePersonalAttendanceEvt extends MouseAdapter implements ActionListener {
 
 	private ManagePersonalAttendance ma;
+
 	public ManagePersonalAttendanceEvt(ManagePersonalAttendance ma) {
-		this.ma=ma;  
-	}//Constructor
-	
+		this.ma = ma;
+	}// Constructor
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
-	}
-	
-}//class
+
+		// 조회버튼
+		if (e.getSource() == ma.getJbtnAttendName()) {
+			
+			// 도장 선언
+			ImageIcon attend = new ImageIcon("C:/Users/user/git/HCYErp/HCYErp/src/image/HCYAttedanceStamp.png");
+			ImageIcon leave = new ImageIcon("C:/Users/user/git/HCYErp/HCYErp/src/image/HCYLeaveStamp.png");
+			ImageIcon dayOff = new ImageIcon("C:/Users/user/git/HCYErp/HCYErp/src/image/HCYDayoffStamp.png");
+			ImageIcon tardy = new ImageIcon("C:/Users/user/git/HCYErp/HCYErp/src/image/HCYTardyStamp.png");
+			JLabel jlblAttend = null;
+			
+			if(ma.getJlblAttendList()==null) {
+			ma.setJlblAttendList(new ArrayList<JLabel>());
+			}//if
+			
+			// 이전에 추가된 도장 제거
+			for (JLabel label : ma.getJlblAttendList()) {
+				ma.remove(label);
+			}//for
+			ma.revalidate();
+			ma.repaint();
+			ma.getJlblAttendList().clear();
+			
+			//도장추가
+			Map<Integer, AttendanceStatus> attendMap = new HashMap<Integer, AttendanceStatus>();
+			try {
+				String emp = ma.getJcbEmp().getSelectedItem().toString();
+				attendMap = ManageAttendanceDAO.getInstance()
+						.selectNoAttend(Integer.parseInt(emp.substring(0,emp.indexOf("/"))));
+			} catch (SQLException se) {
+				JOptionPane.showMessageDialog(jlblAttend, "데이터베이스에 문제가 발생했습니다. 기술팀에 문의해주세요!");
+			} // catch
+
+			Calendar cal = Calendar.getInstance();
+			//상황에 맞는 도장 설정
+			for (int i = 0; i < cal.get(Calendar.DATE); i++) {
+				if (attendMap.get(i + 1) != null) {
+					switch (attendMap.get(i + 1)) {
+					//출근도장 쾅
+					case ATTENDANCE:
+						jlblAttend = new JLabel(attend);
+						jlblAttend.setBounds(ma.getDayList().get(i).getX() - 50, ma.getDayList().get(i).getY() + 15, 50,
+								50);
+						ma.add(jlblAttend);
+						ma.setComponentZOrder(jlblAttend, 0);
+						ma.repaint();
+						ma.getJlblAttendList().add(jlblAttend);
+						break;
+					//지각도장 쾅
+					case ABSENCE:
+						jlblAttend = new JLabel(tardy);
+						jlblAttend.setBounds(ma.getDayList().get(i).getX() - 50, ma.getDayList().get(i).getY() + 15, 50,
+								50);
+						ma.add(jlblAttend);
+						ma.setComponentZOrder(jlblAttend, 0);
+						ma.repaint();
+						ma.getJlblAttendList().add(jlblAttend);
+						break;
+					//휴가도장 쾅
+					case DAY_OFF:
+						jlblAttend = new JLabel(dayOff);
+						jlblAttend.setBounds(ma.getDayList().get(i).getX() - 50, ma.getDayList().get(i).getY() + 15, 50,
+								50);
+						ma.add(jlblAttend);
+						ma.setComponentZOrder(jlblAttend, 0);
+						ma.repaint();
+						ma.getJlblAttendList().add(jlblAttend);
+						break;
+					//휴직도장 쾅
+					case LEAVE:
+						jlblAttend = new JLabel(leave);
+						jlblAttend.setBounds(ma.getDayList().get(i).getX() - 50, ma.getDayList().get(i).getY() + 15, 50,
+								50);
+						ma.add(jlblAttend);
+						ma.setComponentZOrder(jlblAttend, 0);
+						ma.repaint();
+						ma.getJlblAttendList().add(jlblAttend);
+						break;
+					default:
+					}// switch
+				} // if
+			} // for
+		} // if
+	}// actionPerformed
+
+}// class
