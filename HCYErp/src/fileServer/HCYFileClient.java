4137ec19f23fcb788827ac34cb5e7e018d613994
user <user@DESKTOP-COJ9858>
2023-09-15 14:29:04 +0900

    파일 다운로드 완 참조 남음

--- HCYErp/src/fileServer/HCYFileClient.java
+++ HCYErp/src/fileServer/HCYFileClient.java
@@ -1,5 +1,6 @@
 package fileServer;
 
+import java.awt.FileDialog;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,80 +16,84 @@ import java.net.UnknownHostException;
 public class HCYFileClient {
 	private static HCYFileClient hcyFC;
 	private String serverIp = "192.168.10.145";
-	
+
 	private HCYFileClient() {
-	}//constructor
-	
+	}// constructor
+
 	public static HCYFileClient getInstance() {
-		if(hcyFC == null) {
+		if (hcyFC == null) {
 			hcyFC = new HCYFileClient();
-		}//if
+		} // if
 		return hcyFC;
-	}//getInstance
-	
+	}// getInstance
+
 	public void uploadFile(File file) throws UnknownHostException, IOException {
-		Socket socket = new Socket(serverIp,36500);
-		//이름 및 확장자 보내기
+		Socket socket = new Socket(serverIp, 36500);
+		// 이름 및 확장자 보내기
 		String filePath = file.getAbsolutePath();
-		String fileNameWithExtension = filePath.substring(filePath.lastIndexOf("\\")+1); // 파일 이름 및 확장자 설정
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        writer.println(fileNameWithExtension);
-        
-        FileInputStream fis = new FileInputStream(file);
-        OutputStream os = socket.getOutputStream();
-        byte[] buffer = new byte[4096];
-        int bytesRead = 0;
-        
-        while ((bytesRead = fis.read(buffer)) != -1) {
-        	os.write(buffer, 0, bytesRead);
-        }//while
+		String fileNameWithExtension = filePath.substring(filePath.lastIndexOf("\\") + 1); // 파일 이름 및 확장자 설정
+		PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+		writer.println(fileNameWithExtension);
 
-        os.close();
-        fis.close();
-        writer.close();
-        socket.close();
-	}//uploadFile
-	
-	public void downloadFile(File file) throws UnknownHostException, IOException {
-		Socket socket = new Socket(serverIp,36500);
-		//이름 및 확장자 보내기
-		String filePath = file.getAbsolutePath();
-		String fileNameWithExtension = filePath.substring(filePath.lastIndexOf("\\")+1); // 파일 이름 및 확장자 설정
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        writer.println(fileNameWithExtension);
-        
-        FileOutputStream fos = new FileOutputStream(file);
-        InputStream is = socket.getInputStream();
-        byte[] buffer = new byte[4096];
-        int bytesRead = 0;
-        
-        while ((bytesRead = is.read(buffer)) != -1) {
-        	fos.write(buffer, 0, bytesRead);
-        }//while
+		FileInputStream fis = new FileInputStream(file);
+		OutputStream os = socket.getOutputStream();
+		byte[] buffer = new byte[4096];
+		int bytesRead = 0;
 
-        is.close();
-        fos.close();
-        writer.close();
-        socket.close();
-	}//uploadFile
-	
+		while ((bytesRead = fis.read(buffer)) != -1) {
+			os.write(buffer, 0, bytesRead);
+		} // while
+
+		os.close();
+		fis.close();
+		writer.close();
+		socket.close();
+	}// uploadFile
+
+	public void downloadFile(String filePath) throws UnknownHostException, IOException {
+		Socket socket = null;
+		PrintWriter writer = null;
+		FileOutputStream fos = null;
+		InputStream is = null;
+		try {
+			socket = new Socket(serverIp, 36500);
+			// 이름 및 확장자 보내기
+			writer = new PrintWriter(socket.getOutputStream(), true);
+			writer.println(filePath.substring(filePath.lastIndexOf("/") + 1));
+
+			fos = new FileOutputStream(filePath);
+			is = socket.getInputStream();
+			byte[] buffer = new byte[4096];
+			int bytesRead = 0;
+
+			while ((bytesRead = is.read(buffer)) != -1) {
+				fos.write(buffer, 0, bytesRead);
+			} // while
+		} finally {
+			is.close();
+			fos.close();
+			writer.close();
+			socket.close();
+		} // finally
+	}// uploadFile
+
 	public boolean deleteFile(String fileName) throws UnknownHostException, IOException {
 		boolean flag = false;
-		Socket socket = new Socket(serverIp,36700);
-		//이름 및 확장자 보내기
-        PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        writer.println(fileName);
+		Socket socket = new Socket(serverIp, 36700);
+		// 이름 및 확장자 보내기
+		PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+		writer.println(fileName);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        switch (reader.readLine()) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		switch (reader.readLine()) {
 		case "success":
 			flag = true;
 			break;
-		}//switch
-        
-        writer.close();
-        socket.close();
-        
-        return flag;
-	}//deleteFile
-}//class
+		}// switch
+
+		writer.close();
+		socket.close();
+
+		return flag;
+	}// deleteFile
+}// class
