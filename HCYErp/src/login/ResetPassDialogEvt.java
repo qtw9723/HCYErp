2a5c4c4c5f803655459827db866d41dad4c1d241
user <user@DESKTOP-COJ9858>
2023-09-15 13:59:32 +0900

    비번 초기화까지

--- HCYErp/src/login/ResetPassDialogEvt.java
+++ HCYErp/src/login/ResetPassDialogEvt.java
@@ -6,57 +6,78 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
-public class ResetPassDialogEvt extends MouseAdapter implements ActionListener {
-	private ResetPassDialog fpd;
+import VO.EmpVO;
 
-	public ResetPassDialogEvt(ResetPassDialog fpd) {
-		this.fpd = fpd;
-		fpd.addWindowListener(new WindowAdapter() {
+public class ResetPassDialogEvt extends MouseAdapter implements ActionListener {
+	private ResetPassDialog rpd;
+
+	public ResetPassDialogEvt(ResetPassDialog rpd) {
+		this.rpd = rpd;
+		rpd.addWindowListener(new WindowAdapter() {
 
 			@Override
 			public void windowClosing(WindowEvent e) {
-				fpd.dispose();
+				rpd.dispose();
 			}
 			
 		});//addWindowListener
 	}// constructor
 
-	public void resetPass() {
-		if(fpd.getJtfEmpNo().getText().length()==0||fpd.getJtfEname().getText().length()==0||
-				fpd.getJtfSsn().getText().length()==0) {
-			JOptionPane.showMessageDialog(fpd, "값을 입력해주세요");
-		}else {
-			String InputEmpno=String.valueOf(fpd.getInputEmpno());
-			String InputEname=String.valueOf(fpd.getInputEname());
-			String InputSsn=String.valueOf(fpd.getInputSsn());
-			
-			if(fpd.getJtfEmpNo().getText().equals(InputEmpno)&&
-				fpd.getJtfEname().getText().equals(InputEname)&&
-				fpd.getJtfSsn().getText().equals(InputSsn)) {
-				JOptionPane.showMessageDialog(fpd, "맞는 비밀번호다 비밀번호 초기화됨");
-				fpd.getJtfEmpNo().setText("");
-				fpd.getJtfEname().setText("");
-				fpd.getJtfSsn().setText("");
-			}else {
-				JOptionPane.showMessageDialog(fpd, "올바른 정보를 입력하세요");
-			}
-			
+	public void resetPass() throws SQLException {
+		if(rpd.getJtfEmpNo().getText().length()==0||rpd.getJtfEname().getText().length()==0||
+				rpd.getJtfSsn().getText().length()==0) {
+			JOptionPane.showMessageDialog(rpd, "값을 입력해주세요");
+			return;
 		}
+			int empNo=Integer.parseInt(rpd.getJtfEmpNo().getText());
+			String ename=rpd.getJtfEname().getText();
+			String ssn=rpd.getJtfSsn().getText();
+			
+			HCYErpDAO.getInstance().selectLogin(empNo);
+			boolean flag=false;
+			
+
+			flag=HCYErpDAO.getInstance().selectLogin(empNo);
+			
+		    if(flag) {
+		    	EmpVO eVO=new EmpVO();
+		    	//사원번호
+		    	eVO.setEmpNo(empNo);
+		    	
+		    	//이름
+		    	eVO.setEname(ename);
+		    	
+		    	//주민번호
+		    	eVO.setSsn(ssn);
+		    	
+		    	
+		    	HCYErpDAO.getInstance().updatePassword(eVO);
+		    	JOptionPane.showMessageDialog(rpd, "비밀번호가 초기화되었습니다");
+		    
+		    }else {
+		    	JOptionPane.showMessageDialog(rpd, "올바른 정보를 입력하세요");
+		    }
+		    
 	}//resetPass
 	
 	public void cancel() {
-		fpd.dispose();
+		rpd.dispose();
 	}//cancel
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==fpd.getJbtnCancel()) {
+		if(e.getSource()==rpd.getJbtnCancel()) {
 			cancel();
-		}else if(e.getSource()==fpd.getJbtnResetPass()) {
-			resetPass();
+		}else if(e.getSource()==rpd.getJbtnResetPass()) {
+			try {
+				resetPass();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}// actionPerformed
 
