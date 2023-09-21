64facff9ffdbdbcae805f4ef7b5ff87206287187
user <user@DESKTOP-COJ9858>
2023-09-21 09:59:01 +0900

    탭변경될때마다 화면초기화

--- HCYErp/src/login/HCYErpEvt.java
+++ HCYErp/src/login/HCYErpEvt.java
@@ -1,5 +1,7 @@
 package login;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,6 +19,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableCellRenderer;
 
 import VO.EmpVO;
 import attendance.Attendance;
@@ -35,8 +38,16 @@ public class HCYErpEvt extends MouseAdapter implements ActionListener {
 
 	private HCYErp hcyE;
 	private int empNo;
-	private int selectedIndex;
+	private int selectedIndex=0;
 
+	private static final int ManageDoc=1;
+	private static final int ManageDailyReport=3;
+	private static final int ManageEmp=4;
+	private static final int ManageMonthlyAttendance=5;
+	private static final int ManagePersonalAttendance=6;
+	private static final int ManageEmpRegister=7;
+	private static final int ManageLeave=8;
+	
 	public HCYErpEvt(HCYErp hcyE) {
 		this.hcyE = hcyE;
 		hcyE.addWindowListener(new WindowAdapter() {
@@ -70,7 +81,8 @@ public class HCYErpEvt extends MouseAdapter implements ActionListener {
 	public void login() throws SQLException {
 	
 		HCYErpDAO hcyEDAO = HCYErpDAO.getInstance();
-
+		Calendar cal=Calendar.getInstance();
+		
 		char[] passwordChar = hcyE.getJpfPass().getPassword();
 		String password = new String(passwordChar);
 		empNo = Integer.parseInt(hcyE.getJtfEmpNo().getText());
@@ -80,6 +92,8 @@ public class HCYErpEvt extends MouseAdapter implements ActionListener {
 		ManageDailyReport mdr = new ManageDailyReport(hcyE);
 		ManageMonthlyAttendance mma = new ManageMonthlyAttendance(hcyE);
 		ManagePersonalAttendance mpa = new ManagePersonalAttendance(hcyE);
+		ManageEmpRegister mer=new ManageEmpRegister(hcyE);
+		ManageLeave ml=new ManageLeave(hcyE);
 		EmpVO eVO = new EmpVO();
 		AttendanceDAO aDAO = AttendanceDAO.getInstance();
 		eVO = aDAO.selectEmp(hcyE.getUser());
@@ -91,6 +105,58 @@ public class HCYErpEvt extends MouseAdapter implements ActionListener {
 					@Override
 					public void stateChanged(ChangeEvent e) {
 						selectedIndex = hcyE.getTabbedPane().getSelectedIndex();
+						switch (selectedIndex) {
+						case ManageDoc:
+							for(Map.Entry<Integer, JCheckBox> entry : md.getJcheckBoxMap().entrySet()) {
+								entry.getValue().setSelected(false);
+							}
+							break;
+						case ManageDailyReport:
+							
+							mdr.getJcbYear().setSelectedItem(cal.get(Calendar.YEAR));
+							mdr.getJcbMonth().setSelectedItem(cal.get(Calendar.MONTH)+1);
+							mdr.getJcbDay().setSelectedItem(cal.get(Calendar.DAY_OF_MONTH));
+							mdr.getJcbEmp().setSelectedIndex(0);
+							break;
+
+						case ManageEmp:
+							ManageEmpDAO meDAO=ManageEmpDAO.getInstance();
+							me.getDlmDept().removeAllElements();
+							me.getDlmteam().removeAllElements();
+							me.getDlmEmp().removeAllElements();
+							int cnt=0;
+							try {
+								List<String> listName=meDAO.selectDept();
+								for (String dept : listName) {
+									if(dept.substring(0,dept.indexOf("/")).equals("0")) {
+										cnt++;
+										continue;
+									}
+									if(cnt==0) {
+									me.getDlmDept().addElement(dept.substring(dept.indexOf("/")+1));
+									}
+									if(cnt==1) {
+									me.getDlmteam().addElement(dept.substring(dept.indexOf("/")+1));
+									}
+									if(cnt==2) {
+									me.getDlmEmp().addElement(dept.substring(dept.indexOf("/")+1));
+									}
+								}
+							} catch (SQLException se) {
+								se.printStackTrace();
+							}//catch
+						case ManageMonthlyAttendance:
+							mma.getJcbYear().setSelectedItem(cal.get(Calendar.YEAR));
+							mma.getJcbMonth().setSelectedItem(cal.get(Calendar.MONTH)+1);
+						case ManagePersonalAttendance:
+							mpa.getJcbEmp().setSelectedIndex(0);
+						case ManageEmpRegister:
+							mer.getJtRegiAbInfo().clearSelection();
+						case ManageLeave:
+							ml.getJtLeaveProposal().clearSelection();
+						default:
+							break;
+						}
 					}// stateChanged
 				});
 				hcyE.add(hcyE.getTabbedPane());
@@ -102,8 +168,8 @@ public class HCYErpEvt extends MouseAdapter implements ActionListener {
 				hcyE.getTabbedPane().add("월별 사원근태 관리", mma);
 				hcyE.getTabbedPane().add("사원명별 사원근태 관리", mpa);
 				if (eVO.getTeam().equals("인사") || eVO.getTeam().equals("임원")) {
-					hcyE.getTabbedPane().add("입퇴사 관리", new ManageEmpRegister(hcyE));
-					hcyE.getTabbedPane().add("휴가 관리", new ManageLeave(hcyE));
+					hcyE.getTabbedPane().add("입퇴사 관리", mer);
+					hcyE.getTabbedPane().add("휴가 관리", ml);
 				}
 
 //				} // else
@@ -117,6 +183,4 @@ public class HCYErpEvt extends MouseAdapter implements ActionListener {
 	public int getSelectedIndex() {
 		return selectedIndex;
 	}
-
-
 }// class
